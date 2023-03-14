bdf72acae4adaaa7df2da044aa9760d6d243a8b7
sleepyjoyy <sleepyjoyy@gmail.com>
2023-03-14 16:27:48 +0900

    [Feat] Props로 내린 속성 Rest 문법적용, 배우 이름 사용할 수 있게 적용

--- client/src/components/ProfileImg.jsx
+++ client/src/components/ProfileImg.jsx
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 import DefaultProfile from '../assets/DefaultProfile.png';
 
-/** 추후 배우페이지 확장성을 위해 onClick 속성을 프롭스로 연결해두었습니다 */
+/** 공연정보에서 프로필이미지 사용할때는 type=actor, name=배우이름을 넘겨주기 */
 function ProfileImg({
+  type,
+  name,
   src,
   alt,
-  onClick,
   width,
   height,
   borderRadius,
   margin,
-  type,
+  ...rest
 }) {
   /** src에서 이미지를 불러오는데 모종의 이유로 실패할 경우, 디폴트 png 이미지로 설정합니다. */
   const handleImgError = e => {
@@ -18,28 +19,40 @@ function ProfileImg({
   };
 
   return (
-    <ProfileImage
-      src={src}
-      type={type}
-      alt={alt}
-      onClick={onClick}
-      onError={handleImgError}
-      width={width}
-      height={height}
-      borderRadius={borderRadius}
-      margin={margin}
-    />
+    <ProfileContainer>
+      <ProfileImage
+        src={src}
+        type={type}
+        alt={alt}
+        onError={handleImgError}
+        width={width}
+        height={height}
+        borderRadius={borderRadius}
+        margin={margin}
+        {...rest}
+      />
+      {name}
+    </ProfileContainer>
   );
 }
 
+const ProfileContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 /** 공연정보에서 배우프로필용으로 사용할 경우 type="actor" 프롭스를 내려 호버나 액티브 컬러를 지정하지 않습니다.
-나머지 유저 프로필이나, 댓글창 프로필에서 사용할 경우 따로 type을 지정하지 않고 사용합니다.*/
+나머지 유저 프로필이나, 댓글창 프로필에서 사용할 경우 따로 type을 지정하지 않고 사용합니다. */
 const ProfileImage = styled.img`
   src: ${({ src }) => src};
   alt: ${({ alt }) => alt || 'Profile Image'};
   height: ${({ height }) => height || '75px'};
   width: ${({ width }) => width || '75px'};
-  margin: ${({ margin }) => margin || 0};
+  margin: ${({ margin }) => margin || 'var(--font-size-xs)'};
   border: ${({ border }) => border || 'none'};
   border-radius: ${({ borderRadius }) => borderRadius || '50%'};
   :hover {
