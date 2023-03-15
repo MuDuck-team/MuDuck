f0025a54b6a69c24a6cb02c706d98f0dedca8fca
sleepyjoyy <sleepyjoyy@gmail.com>
2023-03-15 10:24:47 +0900

    [Design] 버튼 텍스트 정렬

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-/** - 버튼의 디폴트값 : bgColor 주황색, width는 auto, height 30px 입니다.
+/** - 버튼의 디폴트값 : bgColor 주황색, width는 75px, height 30px 입니다.
  - 변경하여 사용하기 : Button 태그에 프롭스로 text="확인" bgColor="#d3233a" 등으로 내려주세요 */
 function Button({
   type,
@@ -40,16 +40,20 @@ function Button({
 }
 
 const GlobalButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   outline: none;
   :focus {
     outline: none;
   }
   height: ${({ height }) => height || '30px'};
-  width: ${({ width }) => width || 'auto'};
+  width: ${({ width }) => width || '75px'};
   font-size: ${({ fontSize }) => fontSize || 'var(--font-size-md)'};
   font-weight: ${({ fontWeight }) => fontWeight || 600};
   padding: ${({ padding }) => padding || 0};
   border: ${({ border }) => border || 'none'};
+  margin: ${({ margin }) => margin || 'none'};
   border-radius: 8px;
   background-color: ${({ bgColor }) => bgColor || 'var(--button-color)'};
   color: ${({ textColor }) => textColor || 'var(--font-color)'};
