e471f151a42cc871969b9cab57a0f184d1b76b2a
Paksubeen <tnqls520@gmail.com>
2023-03-17 22:26:15 +0900

    [Test] Dummy Data 추가

---
+++ client/src/routes/Mypage/ContentData.js
@@ -0,0 +1,127 @@
+const PostData = [
+  {
+    id: 1,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 2,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 3,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 4,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 5,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 6,
+    nickname: '뮤지컬찐덕후',
+    title: '김코딩 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 7,
+    nickname: '뮤지컬찐덕후',
+    title: '김코딩 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 8,
+    nickname: '뮤지컬찐덕후',
+    title: '김코딩 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 9,
+    nickname: '뮤지컬찐덕후',
+    title: '김코딩 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 10,
+    nickname: '뮤지컬찐덕후',
+    title: '김코딩 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+];
+
+export const CommentData = [
+  {
+    id: 1,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 2,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 3,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 4,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 5,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 6,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 7,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 8,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 9,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+  {
+    id: 10,
+    nickname: '뮤지컬찐덕후',
+    title: '뮤지컬 최고다',
+    lastCreatedAt: '2023.01.02 21:23',
+  },
+];
+
+export default PostData;
