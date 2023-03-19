1a04058b2319cd74ae66431230376fa94d7b0b7d
sleepyjoyy <sleepyjoyy@gmail.com>
2023-03-19 17:09:32 +0900

    [Design] 스크롤바 디자인 변경

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -23,6 +23,25 @@ const GlobalStyle = createGlobalStyle`
     cursor: pointer;
   }
 
+  ::-webkit-scrollbar-track
+{
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  border-radius: 10px;
+  background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar
+{
+  width: 12px;
+  background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar-thumb
+{
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+  background-color: #555;
+}
   
 `;
 
