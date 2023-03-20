12becf4f012ce70cb729e4d8c4bf4dcb078b7bac
Paksubeen <tnqls520@gmail.com>
2023-03-20 17:26:13 +0900

    [Refactor] PostInfo 변수명 변경 및 댓글 삭제 추가

--- client/src/components/PostInfo.jsx
+++ client/src/components/PostInfo.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { BsFillChatFill } from 'react-icons/bs';
-import { FiHeart } from 'react-icons/fi';
+import { AiOutlineHeart } from 'react-icons/ai';
 import { IoMdEye } from 'react-icons/io';
-import ProfileImg from './ProfileImg';
+import ProfileImg from './ProfileImage/ProfileImg';
 import MeatballsMenu from './MeatballsMenu';
 
 function PostInfo({
@@ -10,9 +10,10 @@ function PostInfo({
   nickname,
   createdAt,
   viewCount,
-  chatCount,
-  heartCount,
+  totalComment,
+  boardLike,
   category,
+  type,
 }) {
   return (
     <InformationContainer>
@@ -28,23 +29,23 @@ function PostInfo({
                 <InfoText>{viewCount}</InfoText>
               </InfoBox>
             )}
-            {chatCount && (
+            {totalComment && (
               <InfoBox>
                 <ChatIcon />
-                <InfoText>{chatCount}</InfoText>
+                <InfoText>{totalComment}</InfoText>
               </InfoBox>
             )}
-            {heartCount && (
+            {boardLike && (
               <InfoBox>
                 <HeartIcon />
-                <InfoText>{heartCount}</InfoText>
+                <InfoText>{boardLike}</InfoText>
               </InfoBox>
             )}
             {category && <InfoText>카테고리 | {category}</InfoText>}
           </PostInformation>
         </InformationWrapper>
       </InformationDetail>
-      <MeatballsMenu />
+      {type === 'post' ? <MeatballsMenu /> : <DeleteButton>삭제</DeleteButton>}
     </InformationContainer>
   );
 }
@@ -99,10 +100,21 @@ const ChatIcon = styled(BsFillChatFill)`
   margin-right: 2px;
 `;
 
-const HeartIcon = styled(FiHeart)`
-  width: 12px;
-  height: 12px;
+const HeartIcon = styled(AiOutlineHeart)`
+  width: 14px;
+  height: 14px;
   margin-right: 2px;
+`;
+
+const DeleteButton = styled.button`
+  padding: 0;
+  border: none;
+  background: none;
+  font-size: var(--font-size-xs);
+  color: var(--font-color);
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 export default PostInfo;
