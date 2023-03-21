7f3299cc34e2e869d50f6867004e450de36f0fb1
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-03-22 01:01:41 +0900

    [Feat] kakao에서 muduck 형태로 바꿔주는 함수 추가

--- client/src/utile/changeObjKeys.js
+++ client/src/utile/changeObjKeys.js
@@ -1,15 +1,29 @@
-const changeObjKeys = obj => {
+export const changeMuDuckToKakaoObj = muDuckObj => {
   const kakaoObj = {};
-  kakaoObj.place_name = obj.name;
-  kakaoObj.address_name = obj.address;
-  kakaoObj.road_address_name = obj.roadAddress;
-  kakaoObj.phone = obj.phone;
-  kakaoObj.x = obj.longitude;
-  kakaoObj.y = obj.latitube;
-  kakaoObj.place_url = obj.placeUrl;
-  kakaoObj.score = obj.score;
+  kakaoObj.place_name = muDuckObj.name;
+  kakaoObj.address_name = muDuckObj.address;
+  kakaoObj.road_address_name = muDuckObj.roadAddress;
+  kakaoObj.phone = muDuckObj.phone;
+  kakaoObj.x = muDuckObj.longitude;
+  kakaoObj.y = muDuckObj.latitube;
+  kakaoObj.place_url = muDuckObj.placeUrl;
+  kakaoObj.score = muDuckObj.score;
 
   return kakaoObj;
 };
 
-export default changeObjKeys;
+export const changeKakaoToMuDuckObj = kakaoObj => {
+  const muDuckObj = {};
+  muDuckObj.name = kakaoObj.place_name;
+  muDuckObj.address = kakaoObj.address_name;
+  muDuckObj.roadAddress = kakaoObj.road_address_name;
+  muDuckObj.phone = kakaoObj.phone;
+  muDuckObj.longitude = kakaoObj.x;
+  muDuckObj.latitube = kakaoObj.y;
+  muDuckObj.placeUrl = kakaoObj.place_url;
+  muDuckObj.score = kakaoObj.score;
+  muDuckObj.mapId = kakaoObj.id;
+
+  // category_group_code
+  return muDuckObj;
+};
