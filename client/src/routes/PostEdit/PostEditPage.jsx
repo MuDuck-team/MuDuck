22e7fb55597c1e38c7c0e27108a8e29b311a2914
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-03-25 23:46:44 +0900

    [Feat] PostEdit 기능 구현

--- client/src/routes/PostEdit/PostEditPage.jsx
+++ client/src/routes/PostEdit/PostEditPage.jsx
@@ -1,16 +1,66 @@
-import { useLoaderData } from 'react-router-dom';
+import { redirect, useLoaderData } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import customAxios from '../../api/customAxios';
 import Editors from '../../components/Editors';
+import { userInfo } from '../../recoil/userAtom';
 
-export function loader({ request }) {
-  const url = new URL(request.url);
-  const { pathname } = url;
-  return { pathname };
+export async function loader({ request, params }) {
+  try {
+    const url = new URL(request.url);
+    const paramsId = params.id;
+    console.log(paramsId);
+    const responce = await customAxios.get(`/boards/${paramsId}`);
+    const { pathname } = url;
+
+    return { pathname, responce };
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+export async function action({ request, params }) {
+  try {
+    const formData = await request.formData();
+    const editFormData = Object.fromEntries(formData);
+    delete editFormData.categoryIds;
+    const token = localStorage.getItem('localToken');
+    await customAxios.patch(
+      `/boards/${params.id}`,
+      {
+        ...editFormData,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+
+    return redirect(`/post/${params.id}`);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 }
 
 function PostEditPage() {
-  const { pathname } = useLoaderData();
+  const { pathname, responce } = useLoaderData();
+  const user = useRecoilValue(userInfo);
+  const userId = user?.id;
+  const publisherId = responce.data.boardContent.head.memberId;
+  const { title: defaultTitle, content: defalutContent } =
+    responce.data.boardContent.body;
+  if (!(userId === publisherId)) {
+    redirect(-1);
+  }
 
-  return <Editors pathname={pathname} />;
+  return (
+    <Editors
+      pathname={pathname}
+      defaultTitle={defaultTitle}
+      defalutContent={defalutContent}
+    />
+  );
 }
 
 export default PostEditPage;
