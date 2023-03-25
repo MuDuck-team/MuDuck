29b483de055e9d8b32ad738765104a27382ab9cb
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-03-25 23:46:28 +0900

    [Feat] PostAdd 기능 구현

--- client/src/routes/PostAdd/PostAddPage.jsx
+++ client/src/routes/PostAdd/PostAddPage.jsx
@@ -1,16 +1,45 @@
-import { useLoaderData } from 'react-router-dom';
+import { redirect, useLoaderData } from 'react-router-dom';
+import customAxios from '../../api/customAxios';
 import Editors from '../../components/Editors';
 
-export function loader({ request }) {
+export async function loader({ request }) {
   const url = new URL(request.url);
   const { pathname } = url;
-  return { pathname };
+  const responce = await customAxios.get('/boards/category');
+  const { category, mentionedMusical } = responce.data;
+  return { pathname, category, mentionedMusical };
+}
+
+export async function action({ request }) {
+  try {
+    const formData = await request.formData();
+    const postFormData = Object.fromEntries(formData);
+    const categoryIds = postFormData.categoryIds.split(',').map(Number);
+    const token = localStorage.getItem('localToken');
+    const data = { ...postFormData, categoryIds };
+    await customAxios.post(
+      '/boards',
+      {
+        ...data,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      },
+    );
+
+    return redirect(`/posts`);
+  } catch (err) {
+    console.log(err);
+    throw new Error(err.responce);
+  }
 }
 
 function PostAddPage() {
-  const { pathname } = useLoaderData();
+  const obj = useLoaderData();
 
-  return <Editors pathname={pathname} />;
+  return <Editors {...obj} />;
 }
 
 export default PostAddPage;
