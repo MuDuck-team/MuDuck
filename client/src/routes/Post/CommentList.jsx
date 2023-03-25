ba781db90927c9b379726a22f0282b4f15c4c0f5
Paksubeen <tnqls520@gmail.com>
2023-03-25 16:51:23 +0900

    [Feature] 댓글 등록 기능 구현

--- client/src/routes/Post/CommentList.jsx
+++ client/src/routes/Post/CommentList.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled, { css } from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { userInfo } from '../../recoil/userAtom';
+import customAxios from '../../api/customAxios';
 import WriterInfo from '../../components/WriterInfo';
 import { StyledInput } from '../../components/Input';
 import Button from '../../components/Button';
 
 function CommentList({ comment }) {
-  const { head, body, comments } = comment;
+  const { id, head, body, comments } = comment;
+
+  const params = useParams();
+  const navigate = useNavigate();
+  const localToken = localStorage.getItem('localToken');
+
+  const user = useRecoilValue(userInfo);
+
   const [isReply, setIsReply] = useState(false);
   const [isShowReply, setIsShowReply] = useState(false);
 
@@ -17,9 +28,47 @@ function CommentList({ comment }) {
     setIsShowReply(!isShowReply);
   };
 
+  const [commentValue, setCommentValue] = useState('');
+
+  const handleCommentChange = event => {
+    setCommentValue(event.target.value);
+  };
+
+  const handleCommentSubmit = event => {
+    event.preventDefault();
+
+    if (commentValue === '') {
+      alert('내용을 최소 1자 이상 입력해주세요');
+      return;
+    }
+
+    customAxios
+      .post(
+        `/boards/${params.id}/comments/${id}`,
+        {
+          body: commentValue,
+        },
+        {
+          headers: {
+            Authorization: localToken,
+          },
+        },
+      )
+      .then(response => {
+        console.log(response);
+        setCommentValue('');
+        if (response.status === 201) navigate('.');
+      })
+      .catch(error => {
+        console.error('Error submitting comment:', error);
+      });
+  };
+
   return (
     <CommentContainer>
       <WriterInfo
+        commentId={id}
+        memberId={head.memberId}
         profileUrl={head.userProfile}
         nickname={head.nickname}
         createdAt={head.createdAt}
@@ -33,16 +82,38 @@ function CommentList({ comment }) {
         )}
         <ReplyInput onClick={toggleReplyComment}>댓글 쓰기</ReplyInput>
       </CommentAction>
-      {isReply && (
-        <InputContainer>
-          <StyledInput width="88%" placeholder="댓글을 입력해주세요" />
-          <Button width="10%" height="40px" text="등록" />
-        </InputContainer>
-      )}
+      {isReply &&
+        (user ? (
+          <CommentForm>
+            <StyledInput
+              width="88%"
+              placeholder="댓글을 작성해주세요"
+              value={commentValue}
+              onChange={handleCommentChange}
+            />
+            <Button
+              width="10%"
+              height="40px"
+              text="등록"
+              onClick={handleCommentSubmit}
+            />
+          </CommentForm>
+        ) : (
+          <CommentForm>
+            <StyledInput
+              width="88%"
+              placeholder="로그인 후 이용 가능합니다"
+              disabled
+            />
+            <Button width="10%" height="40px" text="등록" disabled />
+          </CommentForm>
+        ))}
       {isShowReply &&
         comments.map(reply => (
           <ReplyContainer key={reply.id}>
             <WriterInfo
+              commentId={reply.id}
+              memberId={reply.head.memberId}
               profileUrl={reply.head.userProfile}
               nickname={reply.head.nickname}
               createdAt={reply.head.createdAt}
@@ -89,7 +160,7 @@ const ReplyInput = styled.button`
   color: var(--font-color);
 `;
 
-const InputContainer = styled.div`
+const CommentForm = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 1.6rem 0;
