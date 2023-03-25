0bff0cdd268a7ec81728a2a9828bfc50524afee5
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-03-25 11:53:36 +0900

    [Refactor] 더미데이터 삭제

--- client/src/routes/Posts/PostsPage.jsx
+++ client/src/routes/Posts/PostsPage.jsx
@@ -7,69 +7,22 @@ import Paging from '../../components/Pagination/Pagination';
 import Button from '../../components/Button';
 import customAxios from '../../api/customAxios';
 
-// const dummyData = {
-//   noticeBoards: [
-//     {
-//       id: 1,
-//       lastCreatedAt: '약 5시간 전',
-//       title: '[공지사항] 뮤지컬 관람 규칙',
-//     },
-//   ],
-//   boards: [
-//     {
-//       id: 1,
-//       memberId: 1,
-//       nickname: '닉네임',
-//       lastCreatedAt: '약 5시간 전',
-//       userProfile: '유저프로필사진주소',
-//       title: '[자유주제제] 정말 재미 있었어요!',
-//       view: 30,
-//       commentCount: 30,
-//       boardLike: 30,
-//     },
-//     {
-//       id: 2,
-//       memberId: 2,
-//       nickname: '닉네임',
-//       lastCreatedAt: '약 5시간 전',
-//       userProfile: '유저프로필사진주소',
-//       title: '[자유주제] 정말 재미 있었어요!',
-//       view: 30,
-//       commentCount: 30,
-//       boardLike: 30,
-//     },
-//   ],
-//   category: [
-//     { id: 1, categoryName: '자유주제' },
-//     { id: 2, categoryName: '공연정보/후기' },
-//     { id: 3, categoryName: '시설정보' },
-//   ],
-//   pageInfo: {
-//     page: 1,
-//     size: 10,
-//     totalElements: 30,
-//     totalPages: 1,
-//   },
-// };
-
-// function getData({ page, categoryName, sortBy }) {
-//   console.log('aaa', page, categoryName, sortBy);
-//   return dummyData;
-//   // https://axios-http.com/kr/docs/api_intro
-// }
-
 export async function loader({ request }) {
-  const params = new URL(request.url).searchParams;
-  const sortBy = params.get('sortBy') || 'recent';
-  const categoryName = params.get('categoryName') || '자유주제';
-  const page = params.get('page') || '1';
-  // 더미 함수 서버가 나오면 고칠 계획입니다.
-  const obj = await customAxios.get(
-    `/boards?page=${page}&sortBy=${sortBy}&categoryName=${categoryName}`,
-  );
-  const { data } = obj;
-  console.log(data);
-  return { data };
+  try {
+    const params = new URL(request.url).searchParams;
+    const sortBy = params.get('sortBy') || 'recent';
+    const categoryName = params.get('categoryName') || '자유주제';
+    const page = params.get('page') || '1';
+    const obj = await customAxios.get(
+      `/boards?page=${page}&sortBy=${sortBy}&categoryName=${categoryName}`,
+    );
+    const { data } = obj;
+
+    return { data };
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
 }
 
 const sortArr = [
@@ -80,14 +33,13 @@ const sortArr = [
 
 function PostsPage() {
   const { data } = useLoaderData();
-  const { noticeBoards, boards, pageInfo, categoryList } = data;
-
   const navigate = useNavigate();
+
+  const { noticeBoards, boards, pageInfo, categoryList } = data;
   const [currentSortBy, setCurrentSortBy] = useState(sortArr[0]);
   const [currentCategoryName, setCurrentCategoryName] = useState(
     categoryList[0],
   );
-  console.log(Object.keys(boards));
 
   const onClickSortBy = sortObj => {
     setCurrentSortBy(sortObj);
