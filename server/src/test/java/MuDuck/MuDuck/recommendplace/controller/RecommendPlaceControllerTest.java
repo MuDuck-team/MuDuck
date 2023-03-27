f9ae2bc516788eb327cde3f2a2a8f1e4f3c44fd5
kkw <kkte02@naver.com>
2023-03-27 17:45:19 +0900

    [TEST] 평점 한줄평exception 테스트 코드 구현

--- server/src/test/java/MuDuck/MuDuck/recommendplace/controller/RecommendPlaceControllerTest.java
+++ server/src/test/java/MuDuck/MuDuck/recommendplace/controller/RecommendPlaceControllerTest.java
@@ -2,6 +2,8 @@ package MuDuck.MuDuck.recommendplace.controller;
 
 import static MuDuck.MuDuck.utils.ApiDocumentUtils.getRequestPreProcessor;
 import static MuDuck.MuDuck.utils.ApiDocumentUtils.getResponsePreProcessor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
@@ -17,6 +19,8 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import MuDuck.MuDuck.exception.BusinessLogicException;
+import MuDuck.MuDuck.exception.ExceptionCode;
 import MuDuck.MuDuck.map.dto.MapDto;
 import MuDuck.MuDuck.map.entity.Map;
 import MuDuck.MuDuck.map.mapper.MapMapper;
@@ -33,6 +37,7 @@ import MuDuck.MuDuck.recommendplace.service.RecommendPlaceService;
 import MuDuck.MuDuck.response.RecommendPlaceMultipleResponse;
 import com.google.gson.Gson;
 import java.util.List;
+import java.util.Objects;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -196,6 +201,61 @@ class RecommendPlaceControllerTest {
     }
 
     @Test
+    @DisplayName("평점및 한줄평 등록 시 로그인된 회원과 요청 회원이 다를 경우")
+    @WithMockUser
+    void postRecommendPlaceException() throws Exception {
+        // given
+        MapDto.Post mapPost = MapDto.Post.builder()
+                .theaterId(1L)
+                .placeId(2L)
+                .placeName("오둥이식당")
+                .longitude(126.978891)
+                .latitude(37.5709794)
+                .categoryGroupCode("FD6")
+                .phone("02-555-5555")
+                .address("서울 서초구 서초동 700")
+                .roadAddress("서울 서초구 남부순환로 2406")
+                .placeUrl("http://place.map.kakao.com/10753713")
+                .build();
+
+        RecommendPlaceDto.Post rpPost = RecommendPlaceDto.Post.builder()
+                .memberId(2L)
+                .score(4.8)
+                .oneLine("한줄평입네다")
+                .build();
+
+        RecommendPlace recommendPlace = RecommendPlace.builder()
+                .member(Member.builder().memberId(2L).build())
+                .build();
+
+        RecommendPlaceMultipleResponse multipleResponse = new RecommendPlaceMultipleResponse(mapPost, rpPost);
+
+
+        String content = gson.toJson(multipleResponse);
+
+        given(mapMapper.postMapToMap(Mockito.any())).willReturn(Map.builder().build());
+        given(recommendPlaceMapper.postDtoToRecommendPlace(Mockito.any())).willReturn(
+                recommendPlace);
+        given(memberService.findByEmail(Mockito.anyString()))
+                .willReturn(member);
+        // when / then
+        ResultActions actions =
+                mockMvc.perform(
+                        post("/recommend-place")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content).with(csrf())
+                        )
+                        .andExpect(status().isUnauthorized())
+                        .andExpect(
+                                (result) ->
+                                        assertTrue(result.getResolvedException() instanceof BusinessLogicException))
+                        .andExpect(
+                                (result) ->
+                                        assertEquals(ExceptionCode.NOT_SAME_USER.getMessage(), result.getResolvedException().getMessage()));
+    }
+
+    @Test
     @DisplayName("mapId와 memberId로 한줄평 조회")
     @WithMockUser(username = TEST_USER_EMAIL)
     void getRecommendPlaceTest() throws Exception {
@@ -239,6 +299,33 @@ class RecommendPlaceControllerTest {
                         )
                 );
     }
+
+    @Test
+    @DisplayName("평점 및 한줄평 조회시 로그인된 회원과 요청 회원이 다를 경우")
+    @WithMockUser
+    void getRecommendPlaceExceptionTest() throws Exception {
+        // given
+        long mapId = 1L;
+        long anotherId = 2L;
+        given(memberService.findByEmail(Mockito.anyString()))
+                .willReturn(member);
+        given(memberService.findByEmail(Mockito.anyString())).willReturn(member);
+        given(mapService.findVerifiedMapToMapId(Mockito.anyLong())).willReturn(Map.builder().build());
+        given(recommendPlaceService.findRecommendPlaceToMemberIdAndMapId(Mockito.anyLong(), Mockito.anyLong()))
+                .willReturn(RecommendPlace.builder().build());
+        given(recommendPlaceMapper.recommendPlaceToResponse(Mockito.any())).willReturn(rpResponse);
+
+        // when / then
+        ResultActions getActions =
+                mockMvc.perform(
+                        get("/recommend-place/maps/{map-id}/members/{member-id}", mapId, anotherId)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                        .andExpect(status().isUnauthorized())
+                        .andExpect(result -> assertTrue(result.getResolvedException() instanceof BusinessLogicException))
+                        .andExpect(result -> assertEquals(ExceptionCode.NOT_SAME_USER.getMessage(), result.getResolvedException().getMessage()));
+    }
+
     @Test
     @DisplayName("평점 및 한줄평 업데이트 테스트")
     @WithMockUser(username = TEST_USER_EMAIL)
@@ -308,5 +395,79 @@ class RecommendPlaceControllerTest {
                                 )
                         )
                 ));
+    }
+
+    @Test
+    @DisplayName("평점 및 한줄평 업데이트시 로그인 된 유저와 요청 유저가 다를 경우")
+    @WithMockUser
+    void patchRecommendPlaceExceptionTest() throws Exception {
+        // given
+        long rpId = 1L;
+        long mapId = 1L;
+        long anotherId = 2L;
+        RecommendPlace recommendPlace = RecommendPlace.builder().member(member).build();
+
+        RecommendPlaceDto.Patch patch = new Patch(4.5, "한줄평입니다.");
+
+        String content = gson.toJson(patch);
+
+        given(recommendPlaceMapper.patchDtoToRecommendPlace(Mockito.any())).willReturn(RecommendPlace.builder().build());
+        given(memberService.findByEmail(Mockito.anyString())).willReturn(member);
+        given(recommendPlaceService.findRecommendPlaceToMemberIdAndMapId(Mockito.anyLong(),Mockito.anyLong()))
+                .willReturn(recommendPlace);
+
+        given(recommendPlaceService.updateRecommendPlace(Mockito.any())).willReturn(RecommendPlace.builder().build());
+        given(recommendPlaceMapper.recommendPlaceToResponse(Mockito.any())).willReturn(rpResponse);
+
+        // when / then
+        ResultActions actions = mockMvc.perform(
+                patch("/recommend-place/{recommend-place-id}/maps/{map-id}/members/{member-id}",
+                        rpId, mapId, anotherId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content).with(csrf())
+        )
+                .andExpect(status().isUnauthorized())
+                .andExpect(result -> assertTrue(
+                        result.getResolvedException() instanceof BusinessLogicException))
+                .andExpect(result -> assertEquals(ExceptionCode.NOT_SAME_USER.getMessage(),
+                        result.getResolvedException().getMessage()));
+    }
+
+    @Test
+    @DisplayName("변경 요청한 글이 본인 글이 아닐 경우 테스트")
+    @WithMockUser
+    void patchRecommendPlaceExceptionTest2() throws Exception {
+        // given
+        long rpId = 1L;
+        long mapId = 1L;
+        Member anotherMember = Member.builder().memberId(2L).build();
+        RecommendPlace recommendPlace = RecommendPlace.builder().member(anotherMember).build();
+
+        RecommendPlaceDto.Patch patch = new Patch(4.5, "한줄평입니다.");
+
+        String content = gson.toJson(patch);
+
+        given(recommendPlaceMapper.patchDtoToRecommendPlace(Mockito.any())).willReturn(RecommendPlace.builder().build());
+        given(memberService.findByEmail(Mockito.anyString())).willReturn(member);
+        given(recommendPlaceService.findRecommendPlaceToMemberIdAndMapId(Mockito.anyLong(),Mockito.anyLong()))
+                .willReturn(recommendPlace);
+
+        given(recommendPlaceService.updateRecommendPlace(Mockito.any())).willReturn(RecommendPlace.builder().build());
+        given(recommendPlaceMapper.recommendPlaceToResponse(Mockito.any())).willReturn(rpResponse);
+
+        // when / then
+        ResultActions actions = mockMvc.perform(
+                patch("/recommend-place/{recommend-place-id}/maps/{map-id}/members/{member-id}",
+                        rpId, mapId, recommendPlace.getMember().getMemberId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content).with(csrf())
+        )
+                .andExpect(status().isUnauthorized())
+                .andExpect(result -> assertTrue(
+                        result.getResolvedException() instanceof BusinessLogicException))
+                .andExpect(result -> assertEquals(ExceptionCode.NOT_SAME_USER.getMessage(),
+                        result.getResolvedException().getMessage()));
     }
 }
